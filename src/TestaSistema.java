af745c946663f1d922a9ebacf4d5737e2577f828
JSombra <soombra73@gmail.com>
2022-03-25 13:09:44 -0300

    Instanciando outro Administrador

--- src/TestaSistema.java
+++ src/TestaSistema.java
@@ -4,14 +4,16 @@ public class TestaSistema {
         Administrador adm = new Administrador();
         Cliente cliente = new Cliente();
         cliente.setSenha(2222);
+        Administrador adm2 = new Administrador();
 
-
+        adm.setSenha(2221);
         adm.setSenha(2222);
         g.setSenha(2222);
 
         SistemaInterno si = new SistemaInterno();
         si.autentica(adm);
         si.autentica(g);
+        si.autentica(adm2);
         si.autentica(cliente);
     }
 }
